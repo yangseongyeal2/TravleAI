03351dda185026f0ae5c5aa269fecffc526cba63
yangseongyeal2 <ysy96pashon@korea.ac.kr>
2022-10-30 16:04:16 +0900

    Initial Commit

---
+++ TravelAI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelAI
+//
+//  Created by mobile_ on 2022/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
